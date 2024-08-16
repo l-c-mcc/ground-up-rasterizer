56c68af0205609070ccb25cc9ca2d6c7d03d74ed
liamm <131229216+l-c-mcc@users.noreply.github.com>
2024-08-16 12:56:47 -0700

    to-do list to guide self

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use minifb::{Window, WindowOptions};
 
+/*
+rough rendering pipeline:
+- Input assembler (handle geometry)
+- vertex shader
+- rasterize
+- pixel shading
+
+Implementation order?
+1. handling the idea of vertices with associated info (color?) in
+   3d space within the code
+        a. Vertices can be used to form lines and triangles. 
+2. local space -> world space -> view space transformations
+3. rasterization
+ */
+
 fn main() {
-    let width = 500;
-    let height = 500;
+    let width = 1280;
+    let height = 720;
     let buffer: Vec<u32> = vec![255 << 16 | 255 << 8 | 255; width * height];
 
     let mut window = Window::new("Rasterizer", width, height, WindowOptions::default()).unwrap();
