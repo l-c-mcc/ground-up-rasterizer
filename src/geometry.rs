0e9e1adc875b06af360dfd8f9b86e1b10d8d30e0
liamm <131229216+l-c-mcc@users.noreply.github.com>
2024-08-20 10:27:46 -0700

    geometry structure and a basic line

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,11 +1,27 @@
 use nalgebra as na;
 use crate::color::Color;
 
+pub fn line() -> Geometry {
+    let mut line = Geometry::new(GeometryType::Line);
+    line.vertex_locations.push(na::Vector4::new(0.0, 0.0, 0.0, 1.0));
+    line.vertex_locations.push(na::Vector4::new(10.0, 10.0, 0.0, 1.0));
+    line.vertices.push(Vertex::new(0, Color::Red));
+    line.vertices.push(Vertex::new(1, Color::Red));
+    line
+}
+
+/*
+pub fn cube(c: Color) -> Geometry {
+    let mut cube = Geometry::new(GeometryType::Triangle);
+    cube.vertex_locations.push(na::Vector4::new(0.0, 0.0, 0.0, 1.0));
+    cube
+}
+    */
+
 struct Geometry {
-    // (points, color) - vector point type, color
-    vertex_indices: Vec<usize>,
-    vertex_list: Vec<na::Vector4<f32>>,
-    g_type: GeometryType,
+    vertices: Vec<Vertex>,
+    vertex_locations: Vec<na::Vector4<f32>>,
+    geo_type: GeometryType,
 }
 
 struct Vertex {
@@ -16,4 +32,23 @@ struct Vertex {
 enum GeometryType {
     Triangle,
     Line,
+}
+
+impl Geometry {
+    fn new(geo_type: GeometryType) -> Self {
+        Self {
+            vertices: vec![],
+            vertex_locations: vec![],
+            geo_type
+        }
+    }
+}
+
+impl Vertex {
+    fn new(index: usize, color: Color) -> Self {
+        Self {
+            index,
+            color,
+        }
+    }
 }
