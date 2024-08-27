82c5e1b5304abeafb7f5a17b6642285fc99c9e9d
liamm <131229216+l-c-mcc@users.noreply.github.com>
2024-08-27 11:22:26 -0700

    line drawing now handles all lines within bounds

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -36,32 +36,50 @@ pub fn rasterize_geometry(geometry: Vec<Geometry>) -> Vec<ToDraw> {
 /// Uses Bresenham's; look into Wu's for anti-aliasing
 /// To-do: handle vert/horiz lines
 /// To-do: Turn into Result?
-/// To-do: swap is x0 > x1
 /// To-do: color interp
 fn draw_line(line: Geometry) -> Vec<ToDraw> {
-    let mut vertex1 = &line.vertex_locations[line.vertices[0].index];
-    let mut vertex1_color = &line.vertices[0].color;
-    let mut vertex2 = &line.vertex_locations[line.vertices[1].index];
-    let mut vertex2_color = &line.vertices[1].color;
-    if vertex1[0] > vertex2[0] {
-        swap(&mut vertex1, &mut vertex2);
-        swap(&mut vertex1_color, &mut vertex2_color);
+    let vertex1 = &line.vertex_locations[line.vertices[0].index];
+    let vertex2 = &line.vertex_locations[line.vertices[1].index];
+    let vertex1_color = &line.vertices[0].color;
+    let vertex2_color = &line.vertices[1].color;
+    let mut x0 = vertex1[0];
+    let mut y0 = vertex1[1];
+    let mut x1 = vertex2[0];
+    let mut y1 = vertex2[1];
+    let mut y_diff = y1 - y0;
+    let mut x_diff = x1 - x0;
+    let xy_flipped;
+    if y_diff > x_diff {
+        swap(&mut x_diff, &mut y_diff);
+        swap(&mut x0, &mut y0);
+        swap(&mut x1, &mut y1);
+        xy_flipped = true;
+    } else {
+        xy_flipped = false;
     }
-    let y_diff = vertex2[1] - vertex1[1];
-    let x_diff = vertex2[0] - vertex1[0];
+    if x0 > x1 {
+        swap(&mut x0, &mut x1);
+        swap(&mut y0, &mut y1);
+        x_diff *= -1.0;
+        y_diff *= -1.0;
+    }
     let slope = y_diff / x_diff;
-    let y_intercept = (vertex1[1] - slope * vertex1[0]).round() as i32;
+    let y_intercept = (y0 - slope * x0).round() as i32;
     let imp_line = |x: i32, y: i32| {
         (y_diff as i32 * x) - (x_diff as i32 * y) + (x_diff as i32 * y_intercept)
     };
-    let mut y = vertex1[1] as i32;
+    let mut y = y0 as i32;
     let mut draw_buffer = vec![];
-    for x in (vertex1[0].round() as i32)..=(vertex2[0].round() as i32) {
+    for x in (x0.round() as i32)..=(x1.round() as i32) {
         let d = imp_line(2*x, y);
         if d > 0 {
             y += 1;
         }
-        draw_buffer.push(ToDraw::new(x, y, *vertex1_color));
+        if xy_flipped {
+            draw_buffer.push(ToDraw::new(y,x, *vertex1_color));
+        } else {
+            draw_buffer.push(ToDraw::new(x, y, *vertex1_color));
+        }
     }
     draw_buffer
 }
